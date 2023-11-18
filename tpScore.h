4a105301747b222b3068dec6a7c2936c0b5e9660
guiste10 <guiste10@gmail.com>
2023-11-18 13:08:49 +0100

    remove unused field

--- tpScore.h
+++ tpScore.h
@@ -5,14 +5,13 @@
 #include <stdint.h>
 #include "position.h"
 
-#define TP_SCORE_SIZE_LOG2 20 // 2^20 => 1 million entries x 20 bytes x maxdepth x 2 = 640MB, 2^25 => 33.5 million entries x 20 bytes x 16 x 2 = 21GB,
+#define TP_SCORE_SIZE_LOG2 20 // 2^20 => 1 million entries x 16 bytes x maxdepth x 2 = 512MB, 2^25 => 33.5 million entries x 16 bytes x 16 x 2 = 16.8GB,
 #define TP_SCORE_SIZE (1 << TP_SCORE_SIZE_LOG2) // Calculate table size as a power of 2
 
-typedef struct { // 20 bytes
+typedef struct { // 16 bytes
     uint64_t hash;
     int lowerBound;
     int upperBound;
-    int plyStored;
 } TpScoreEntry;
 
 extern TpScoreEntry* tpScore; // indexed by hash, depth and canNullMove
